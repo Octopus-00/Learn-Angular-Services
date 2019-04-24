2cce543c51ffd60c364fd02efd54a18f10c9e7d6
onuroktay <ond@x-t.ch>
2019-04-24 16:34:54 +0200

    Section 2 Defining the Input and Output

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {Weather} from '../shared/weater.model';
+import {WeatherService} from '../shared/weather.service';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent implements OnInit {
+
+  weather: Weather[];
+
+  constructor(private weatherService: WeatherService) {
+  }
+
+  ngOnInit() {
+    this.getWeather();
+  }
+
+  getWeather() {
+    this.weather = this.weatherService.getWeather();
+  }
+
+}
