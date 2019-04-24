1e083497b5b3407c447b94ef7bf661f0a3554cec
onuroktay <ond@x-t.ch>
2019-04-24 16:00:13 +0200

    Section 2 Creating a custom Angular Service - SET and GET Methods

---
+++ src/app/shared/mock-weater.ts
@@ -0,0 +1,26 @@
+export const WEATHER = [
+  {
+    id: 1,
+    date: '2019-08-22T14:40:52.176+00:00',
+    city: 'London',
+    temp: 10,
+    forecast: 'Overcast with showers',
+    home: true
+  },
+  {
+    id: 2,
+    date: '2019-09-22T14:40:52.176+00:00',
+    city: 'New York',
+    temp: 5,
+    forecast: 'Occasional snow',
+    home: false
+  },
+  {
+    id: 3,
+    date: '2019-01-22T14:40:52.176+00:00',
+    city: 'paris',
+    temp: 7,
+    forecast: 'Windy with a chance of rain',
+    home: false
+  }
+];
