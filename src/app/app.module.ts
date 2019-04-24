76f8559f6cd42a2181145aa8f5ff2d084dfa9424
onuroktay <ond@x-t.ch>
2019-04-24 16:34:22 +0200

    Section 2 Defining the Input and Output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {LoggingService} from './shared/logging.service';
+import { WeatherComponent } from './weather/weather.component';
+import { WeatherDetailComponent } from './weather/weather-detail/weather-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    WeatherDetailComponent
   ],
   imports: [
     BrowserModule,
